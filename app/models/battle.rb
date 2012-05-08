b527818423f3f8d15ec4c4f34112b55544165622
Jonatan Brown <jonatanbrown@gmail.com>
2012-05-08 23:05:08 +0200

    Fixed bug in action resolving.

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -54,11 +54,10 @@ class Battle
   end
 
   def self.resolve_action(action)
-    char = action['char']
-    target = action['target']['team'].get_char(action['target']['pos'])
+    char = Character.find(action['char']._id)
+    target = Character.find(action['target']['team'].get_char(action['target']['pos'])._id)
     result = ''
     if char.active and target.active
-      puts "#{char.name} is using #{Constant.get_skill_name(action['skill'].to_i)} on #{target.name} at prio #{action['prio']}"
       case action['skill']
       when '1'
         damage = 20
@@ -91,10 +90,4 @@ class Battle
   end
 
 end
-
-#Action is
-#{"target"=>{"team"=>1, "pos"=>"2"}, "skill"=>"1", "char"=>#<Character _id: 4fa020f247911507ca000022, _type: nil, team_id: BSON::ObjectId('4fa020f247911507ca00001d'), name: "FF 5", str: 10, dex: 7, tgh: 13, ini: 19, int: 12, mem: 5, ap: 9, strike: 6, thrown: 6, projectile: 3, dodge: 9, block: 8, poison: 7, dirt: 1, defensive: 3, destructive: 4, buffs: 9, debuffs: 10, position: 4>, "prio"=>19}
-
-#Action is
-#{"target"=>{"team"=>2, "pos"=>"2"}, "skill"=>"2", "char"=>#<Character _id: 4fa020e247911507ca000016, _type: nil, team_id: BSON::ObjectId('4fa020e247911507ca000015'), name: "C 1", str: 11, dex: 7, tgh: 14, ini: 10, int: 11, mem: 7, ap: 11, strike: 4, thrown: 10, projectile: 10, dodge: 7, block: 2, poison: 10, dirt: 6, defensive: 9, destructive: 7, buffs: 1, debuffs: 3, position: 0>, "prio"=>10}
 
