54e00965e21044b4ca25556ef15f99d2c259d769
Jonatan Brown <jonatanbrown@gmail.com>
2012-07-09 14:35:44 +0200

    Added battle_button helper. Added battle button instead of queue button if in battle in home screen.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
+
+  def battle_button(text)
+    if current_user.battle and current_user.battle.submitted
+      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, waiting_for_turn_path
+    elsif current_user.battle and !current_user.battle.submitted and current_user.battle_sync
+      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, next_turn_path
+    elsif current_user.battle and !current_user.battle.submitted
+      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, battle_path
+    end
+  end
+
 end
 
