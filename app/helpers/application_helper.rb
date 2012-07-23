3e7faa03328ae8915d5446fd7d84bd2d6b9b34fa
Jonatan Brown <jonatanbrown@gmail.com>
2012-07-23 16:03:08 +0200

    Added battle_button to team info panel.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
 
-  def battle_button(text)
+  def battle_button
     if current_user.battle and current_user.battle.submitted
-      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, waiting_for_turn_path
+      link_to "<img src='/images/red_swords.png' border='0'/>".html_safe, waiting_for_turn_path
     elsif current_user.battle and !current_user.battle.submitted and current_user.battle_sync
-      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, next_turn_path
+      link_to "<img src='/images/red_swords.png' border='0'/>".html_safe, next_turn_path
     elsif current_user.battle and !current_user.battle.submitted
-      link_to ("<span class='red'><b>" + text + "</b></span>").html_safe, battle_path
+      link_to "<img src='/images/red_swords.png' border='0'/>".html_safe, battle_path
+    else
+      "<img src='/images/grey_swords.png' border='0'/>".html_safe
     end
   end
 
