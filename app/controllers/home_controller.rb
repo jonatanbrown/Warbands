46fe8b7db55ac1886856eaee05e3f355ed10e006
Jonatan Brown <jonatanbrown@gmail.com>
2012-07-25 14:37:13 +0200

    Fixed Arena button redirect.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,15 @@ class HomeController < ApplicationController
   end
 
   def arena
-
+    if current_user.battle_queue
+      redirect_to queue_path
+    elsif current_user.battle and current_user.battle.submitted
+      redirect_to waiting_for_turn_path
+    elsif current_user.battle and !current_user.battle.submitted and current_user.battle_sync
+      redirect_to next_turn_path
+    elsif current_user.battle and !current_user.battle.submitted
+      redirect_to battle_path
+    end
   end
 end
 
